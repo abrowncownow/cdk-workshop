04bab5cce21907e7406124faf9dc1ab098553b51
Ale Brown <abrowncownow@gmail.com>
2023-06-17 14:55:21 -0700

    added lamda function

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
-
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,   //environment
+      code: lambda.Code.fromAsset('lambda'), //importing code from /lambda
+      handler: 'hello.handler'               //file to load
+    })
   }
 }
