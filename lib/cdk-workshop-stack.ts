0db889109786d13370b53444eb59912276a62f4f
Ale Brown <abrowncownow@gmail.com>
2023-06-18 14:10:58 -0700

    added rest api

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,13 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //lambda function
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_16_X,   //environment
       code: lambda.Code.fromAsset('lambda'), //importing code from /lambda
       handler: 'hello.handler'               //file to load
-    })
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
